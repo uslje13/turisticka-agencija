530aab8cde964fc67aeebeedf09d82462c14bdfb
somelijer <ivan.mikic169@gmail.com>
2023-03-03 18:52:06 +0100

    Added accommodation model
    Added class with properties and CSV functions

---
+++ TravelAgency/Model/Accommodation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.Model
+{
+    internal class Accommodation
+    {
+        public enum AccommodationType
+        {
+            APARTMENT, HOUSE, HUT
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Town { get; set; }
+        public string Country { get; set; }
+        public AccommodationType Type { get; set; }
+        public int MaxGuests { get; set; }
+        public int MinDaysStay { get; set; }
+        public int MinDaysForCancelation { get; set; }
+        //TODO: Url slika
+        public Accommodation()
+        {
+
+        }
+        public Accommodation(int id, string name, AccommodationType type, string town, string country, int maxGuests, int minDaysStay, int minDaysForCancelation = 1)
+        {
+            Id = id;
+            Name = name;
+            Town = town;
+            Country = country;
+            Type = type;
+            MaxGuests = maxGuests;
+            MinDaysStay = minDaysStay;
+            MinDaysForCancelation = minDaysForCancelation;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), Name, Town, Country, Type.ToString(), MaxGuests.ToString(), MinDaysStay.ToString(), MinDaysForCancelation.ToString() };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            int i = 0;
+            Id = Convert.ToInt32(values[i++]);
+            Name = values[i++];
+            Town = values[i++];
+            Country = values[i++];
+            Type = (AccommodationType)Convert.ToInt32(values[i++]);
+            MaxGuests = Convert.ToInt32(values[i++]);
+            MinDaysStay = Convert.ToInt32(values[i++]);
+            MinDaysForCancelation = Convert.ToInt32(values[i++]);
+        }
+    }
+}
