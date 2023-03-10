93acd06cef11d6b15cbd188b6225152329542a58
uslje13 <116906186+uslje13@users.noreply.github.com>
2023-03-10 18:59:58 +0100

    Today Tours View

--- TravelAgency/View/TodayToursView.xaml.cs
+++ TravelAgency/View/TodayToursView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TravelAgency.Model;
 
 namespace TravelAgency.View
 {
@@ -19,9 +21,10 @@ namespace TravelAgency.View
     /// </summary>
     public partial class TodayTourView : Window
     {
-        public TodayTourView()
+        public TodayTourView(ObservableCollection<Tour> tours)
         {
             InitializeComponent();
+            DataContext = this;
         }
     }
 }
