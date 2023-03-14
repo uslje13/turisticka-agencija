57311c7121b72507d85a2291a82f9c37616c5183
somelijer <ivan.mikic169@gmail.com>
2023-03-14 13:33:16 +0100

    Create locationDTO

---
+++ TravelAgency/Model/DTO/LocationDTO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Model;
+using TravelAgency.Repository;
+
+
+namespace TravelAgency.Model.DTO
+{
+    
+    public class LocationDTO
+    {
+        
+        private LocationRepository _locationRepository;
+        public LocationDTO()
+        {
+            _locationRepository = new();
+        }
+
+        public List<string> GetFullNames() 
+        {
+             List<Location> locations = _locationRepository.GetAll();
+            List<string> fullNames = new List<string>();
+            foreach (Location location in locations)
+            {
+                string fullName = location.City + " (" + location.Country + ")";
+                fullNames.Add(fullName);
+            }
+
+            return fullNames;
+        }
+
+        public string GetFullNameById(int locationId) 
+        {
+            Location location = _locationRepository.GetById(locationId);
+            string fullName = location.City + " (" + location.Country + ")";
+
+            return fullName;
+        }
+    }
+}
