0276b0e7cbed1a64ef25bf1b2ef51dcd9ac13a73
draskonovakovic <116906185+draskonovakovic@users.noreply.github.com>
2023-04-08 15:01:59 +0200

    ViewModel opsta klasa

--- TravelAgency/Application/Services/ReservationService.cs
+++ TravelAgency/Application/Services/ReservationService.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SOSTeam.TravelAgency.Domain;
+using SOSTeam.TravelAgency.Domain.Models;
+using SOSTeam.TravelAgency.Domain.RepositoryInterfaces;
 
 namespace SOSTeam.TravelAgency.Application.Services
 {
-    internal class ReservationService
+    public class ReservationService
     {
+        private readonly IReservationRepository _reservationRepository = Injector.CreateInstance<IReservationRepository>();
+        public ReservationService() { }
+
+        public void Delete(int id)
+        {
+            _reservationRepository.Delete(id);
+        }
+
+        public List<Reservation> GetAll()
+        {
+            return _reservationRepository.GetAll();
+        }
+
+        public Reservation GetById(int id)
+        {
+            return _reservationRepository.GetById(id);
+        }
+
+        public void Update(Reservation reservation)
+        {
+            _reservationRepository.Update(reservation);
+        }
+
+        public void Save(Reservation reservation)
+        {
+            _reservationRepository.Save(reservation);
+        }
     }
 }
--- TravelAgency/Application/Services/UserService.cs
+++ TravelAgency/Application/Services/UserService.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SOSTeam.TravelAgency.Domain;
+using SOSTeam.TravelAgency.Domain.Models;
+using SOSTeam.TravelAgency.Domain.RepositoryInterfaces;
 
 namespace SOSTeam.TravelAgency.Application.Services
 {
-    internal class UserService
+    public class UserService
     {
+        private readonly IUserRepository _userRepository = Injector.CreateInstance<IUserRepository>();
+
+        public UserService() { }
+
+        public void Delete(int id)
+        {
+            _userRepository.Delete(id);
+        }
+
+        public List<User> GetAll()
+        {
+            return _userRepository.GetAll();
+        }
+
+        public User GetById(int id)
+        {
+            return _userRepository.GetById(id);
+        }
+
+        public void Save(User user)
+        {
+            _userRepository.Save(user);
+        }
+
+        public void Update(User user)
+        {
+            _userRepository.Update(user);
+        }
+
+        public User GetByUsername(string username)
+        {
+           return _userRepository.GetByUsername(username);
+        }
     }
 }
