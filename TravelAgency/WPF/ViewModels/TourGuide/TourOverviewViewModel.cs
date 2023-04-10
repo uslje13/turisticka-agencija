39c599054c37f374c94121699365723125c8a623
uslje13 <116906186+uslje13@users.noreply.github.com>
2023-04-10 16:42:42 +0200

    Create view cards
    -Added view cards for tour overview

--- TravelAgency/WPF/ViewModels/TourGuide/TourOverviewViewModel.cs
+++ TravelAgency/WPF/ViewModels/TourGuide/TourOverviewViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SOSTeam.TravelAgency.WPF.ViewModels
+namespace SOSTeam.TravelAgency.WPF.ViewModels.TourGuide
 {
     public class TourOverviewViewModel
     {
+        public ObservableCollection<TourCardOverviewViewModel> _tours;
+
         public TourOverviewViewModel() { }
+
+
     }
 }
