d8c471836b150fa9ab4683f38bc4ccf4c9f13d3b
draskonovakovic <116906185+draskonovakovic@users.noreply.github.com>
2023-05-15 23:44:26 +0200

    Statistics almost finished

--- TravelAgency/WPF/ViewModels/Guest2/StatisticsPageViewModel.cs
+++ TravelAgency/WPF/ViewModels/Guest2/StatisticsPageViewModel.cs
@@ -4,6 +4,7 @@ using SOSTeam.TravelAgency.Application.Services;
 using SOSTeam.TravelAgency.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +15,45 @@ namespace SOSTeam.TravelAgency.WPF.ViewModels.Guest2
     {
         public User LoggedInUser { get; set; }
 
+        public SeriesCollection RequestsPercentagePerYear { get; set; }
+        public SeriesCollection RequestsPercentage { get; set; }
         public float AcceptedRequestsPercentage { get; set; }
         public float InvalidRequestsPercentage { get; set; }
         public float AverageTouristNum { get; set; }
-        public float AcceptedRequestsPercentageByYear { get; set; }
-        public float InvalidRequestsPercentageByYear { get; set; }
-        public float AverageTouristNumByYear { get; set; }
+
+        private float _acceptedRequestsPercentageByYear;
+        public float AcceptedRequestsPercentageByYear
+        {
+            get { return _acceptedRequestsPercentageByYear; }
+            set
+            {
+                _acceptedRequestsPercentageByYear = value;
+                OnPropertyChanged(nameof(AcceptedRequestsPercentageByYear));
+            }
+        }
+
+        private float _invalidRequestsPercentageByYear;
+        public float InvalidRequestsPercentageByYear
+        {
+            get { return _invalidRequestsPercentageByYear; }
+            set
+            {
+                _invalidRequestsPercentageByYear = value;
+                OnPropertyChanged(nameof(InvalidRequestsPercentageByYear));
+            }
+        }
+
+        private float _averageTouristNumByYear;
+        public float AverageTouristNumByYear
+        {
+            get { return _averageTouristNumByYear; }
+            set
+            {
+                _averageTouristNumByYear = value;
+                OnPropertyChanged(nameof(AverageTouristNumByYear));
+            }
+        }
+        public ObservableCollection<int> AvailableYears { get; set; }
 
         private int _selectedYear;
 
@@ -29,6 +63,7 @@ namespace SOSTeam.TravelAgency.WPF.ViewModels.Guest2
             set
             {
                 _selectedYear = value;
+                GenerateStatisticsByYear(SelectedYear);
                 OnPropertyChanged(nameof(SelectedYear));
             }
         }
@@ -59,26 +94,62 @@ namespace SOSTeam.TravelAgency.WPF.ViewModels.Guest2
 
         public StatisticsPageViewModel(User loggedInUser)
         {
+            RequestsPercentagePerYear = new SeriesCollection();
+            RequestsPercentage = new SeriesCollection();
+            TourRequestsByLanguage = new SeriesCollection();
+            TourRequestsByLocation = new SeriesCollection();
             LoggedInUser = loggedInUser;
             _tourRequestService = new TourRequestService();
-            SelectedYear = 2024;
+            AvailableYears = new ObservableCollection<int>();
+            SelectedYear = 2023;
+            GetAvailableYears();
             GenerateGeneralStatistics();
             GenerateStatisticsByYear(SelectedYear);
             GenerateTourRequestsByLanguage();
             GenerateTourRequestsByLocation();
         }
 
-        private void GenerateChartData(Dictionary<string, int> dataCounts, ref SeriesCollection series)
+        private void GetAvailableYears()
         {
-            foreach (var dataCount in dataCounts)
+            ObservableCollection<int> availableYears = new ObservableCollection<int>();
+
+            foreach (var request in _tourRequestService.GetAll())
             {
-                series.Add(new PieSeries
-                {
-                    Title = dataCount.Key,
-                    Values = new ChartValues<int> { dataCount.Value },
-                    DataLabels = true
-                });
+                availableYears.Add(request.MaintenanceStartDate.Year);
+                availableYears.Add(request.MaintenanceEndDate.Year);
             }
+
+            var distinctYears = availableYears.Distinct().ToList();
+
+            availableYears.Clear();
+
+            foreach (var year in distinctYears)
+            {
+                AvailableYears.Add(year);
+            }
+        }
+
+        private SeriesCollection GenerateChartData(Dictionary<string, int> dataCounts)
+        {
+            SeriesCollection series = new SeriesCollection();
+
+            var labels = dataCounts.Keys.ToArray();
+            var values = dataCounts.Values.ToArray();
+
+            var columnSeries = new ColumnSeries
+            {
+                Values = new ChartValues<int>(values),
+                DataLabels = true
+            };
+
+            Axis axis = new Axis
+            {
+                Labels = labels
+            };
+
+            series.Add(columnSeries);
+
+            return series;
         }
 
         private void GenerateTourRequestsByLocation()
@@ -87,18 +158,18 @@ namespace SOSTeam.TravelAgency.WPF.ViewModels.Guest2
 
             foreach (var request in _tourRequestService.GetAll())
             {
-                string location = request.City + ", " + request.Country;
+                if(request.UserId == LoggedInUser.Id)
+                {
+                    string location = request.City + ", " + request.Country;
 
-                if (locationCounts.ContainsKey(location))
-                    locationCounts[location]++;
-                else
-                    locationCounts[location] = 1;
+                    if (locationCounts.ContainsKey(location))
+                        locationCounts[location]++;
+                    else
+                        locationCounts[location] = 1;
+                }
             }
 
-            SeriesCollection series = new SeriesCollection();
-            GenerateChartData(locationCounts, ref series);
-
-            TourRequestsByLocation = series;
+            TourRequestsByLocation = GenerateChartData(locationCounts);
         }
 
         private void GenerateTourRequestsByLanguage()
@@ -115,10 +186,7 @@ namespace SOSTeam.TravelAgency.WPF.ViewModels.Guest2
                     languageCounts[language] = 1;
             }
 
-            SeriesCollection series = new SeriesCollection();
-            GenerateChartData(languageCounts, ref series);
-
-            TourRequestsByLanguage = series;
+            TourRequestsByLanguage = GenerateChartData(languageCounts);
         }
         private void GenerateStatistics(int selectedYear = 0)
         {
@@ -129,7 +197,7 @@ namespace SOSTeam.TravelAgency.WPF.ViewModels.Guest2
 
             foreach (var request in _tourRequestService.GetAll())
             {
-                if (request.UserId == LoggedInUser.Id && (selectedYear == 0 || request.MaintenanceStartDate.Year == selectedYear))
+                if (request.UserId == LoggedInUser.Id && (selectedYear == 0 || request.MaintenanceStartDate.Year == selectedYear || request.MaintenanceEndDate.Year == selectedYear))
                 {
                     if (request.Status == StatusType.ACCEPTED)
                     {
@@ -157,6 +225,44 @@ namespace SOSTeam.TravelAgency.WPF.ViewModels.Guest2
                 AcceptedRequestsPercentageByYear = userRequestsNum == 0 ? 0 : (float)acceptedRequests / userRequestsNum * 100;
                 InvalidRequestsPercentageByYear = userRequestsNum == 0 ? 0 : (float)invalidRequests / userRequestsNum * 100;
             }
+
+            if(selectedYear== 0)
+            {
+                RequestsPercentage = new SeriesCollection
+                {
+                    new PieSeries
+                    {
+                        Title = "Prihvaceni zahtevi",
+                        Values = new ChartValues<float> { AcceptedRequestsPercentage },
+                        DataLabels = true
+                    },
+                    new PieSeries
+                    {
+                        Title = "Odbijeni zahtevi",
+                        Values = new ChartValues<float> { InvalidRequestsPercentage },
+                        DataLabels = true
+                    }
+                };
+            }
+            else
+            {
+                RequestsPercentagePerYear = new SeriesCollection
+                {
+                    new PieSeries
+                    {
+                        Title = "Prihvaceni zahtevi",
+                        Values = new ChartValues<float> { AcceptedRequestsPercentageByYear },
+                        DataLabels = true
+                    },
+                    new PieSeries
+                    {
+                        Title = "Odbijeni zahtevi",
+                        Values = new ChartValues<float> { InvalidRequestsPercentageByYear },
+                        DataLabels = true
+                    }
+                };
+            }
+
         }
 
         private void GenerateStatisticsByYear(int selectedYear)
