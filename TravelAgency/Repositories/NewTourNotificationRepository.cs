0a3aa17be28c9c6a7c344891c278fd1ee0575565
uslje13 <116906186+uslje13@users.noreply.github.com>
2023-05-17 10:54:00 +0200

    example of csv
    Csv file for newTourRequests

--- TravelAgency/Repositories/NewTourNotificationRepository.cs
+++ TravelAgency/Repositories/NewTourNotificationRepository.cs
@@ -11,49 +11,76 @@ namespace SOSTeam.TravelAgency.Repositories
 {
     public class NewTourNotificationRepository : INewTourNotificationRepository
     {
-        private const string FilePath = "../../../Resources/Data/images.csv";
+        private const string FilePath = "../../../Resources/Data/newTourNotifications.csv";
 
         private readonly Serializer<NewTourNotification> _serializer;
 
         private List<NewTourNotification> _newTourNotifications;
+
+        public NewTourNotificationRepository()
+        {
+            _serializer = new Serializer<NewTourNotification>();
+            _newTourNotifications = _serializer.FromCSV(FilePath);
+        }
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            _newTourNotifications = _serializer.FromCSV(FilePath);
+            var founded = _newTourNotifications.Find(n=> n.Id == id);
+            _newTourNotifications.Remove(founded);
+            _serializer.ToCSV(FilePath, _newTourNotifications);
         }
 
         public List<NewTourNotification> GetAll()
         {
-            throw new NotImplementedException();
+            return _serializer.FromCSV(FilePath);
         }
 
         public List<NewTourNotification> GetAllByGuestId(int guestId)
         {
-            throw new NotImplementedException();
+            _newTourNotifications = _serializer.FromCSV(FilePath);
+            return _newTourNotifications.FindAll(r => r.GuestId == guestId);
         }
 
         public NewTourNotification? GetById(int id)
         {
-            throw new NotImplementedException();
+            _newTourNotifications = _serializer.FromCSV(FilePath);
+            return _newTourNotifications.FirstOrDefault(r => r.Id == id);
         }
 
         public int NextId()
         {
-            throw new NotImplementedException();
+            _newTourNotifications = _serializer.FromCSV(FilePath);
+            if (_newTourNotifications.Count < 1)
+            {
+                return 1;
+            }
+            return _newTourNotifications.Max(t => t.Id) + 1;
         }
 
         public void Save(NewTourNotification entity)
         {
-            throw new NotImplementedException();
+            entity.Id = NextId();
+            _newTourNotifications = _serializer.FromCSV(FilePath);
+            _newTourNotifications.Add(entity);
+            _serializer.ToCSV(FilePath, _newTourNotifications);
         }
 
         public void SaveAll(List<NewTourNotification> notifications)
         {
-            throw new NotImplementedException();
+            foreach (var notification in notifications)
+            {
+                Save(notification);
+            }
         }
 
         public void Update(NewTourNotification entity)
         {
-            throw new NotImplementedException();
+            _newTourNotifications = _serializer.FromCSV(FilePath);
+            var current = _newTourNotifications.Find(t => t.Id == entity.Id) ?? throw new ArgumentException();
+            int index = _newTourNotifications.IndexOf(current);
+            _newTourNotifications.Remove(current);
+            _newTourNotifications.Insert(index, entity);
+            _serializer.ToCSV(FilePath, _newTourNotifications);
         }
     }
 }
