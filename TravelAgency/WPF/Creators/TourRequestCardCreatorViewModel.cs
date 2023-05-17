c43d920703e4245df830525a30154e46b418e764
uslje13 <116906186+uslje13@users.noreply.github.com>
2023-05-18 00:32:44 +0200

    Accept tour
    accept tour

--- TravelAgency/WPF/Creators/TourRequestCardCreatorViewModel.cs
+++ TravelAgency/WPF/Creators/TourRequestCardCreatorViewModel.cs
@@ -40,47 +40,5 @@ namespace SOSTeam.TravelAgency.WPF.Creators
             return tourRequestCards;
         }
 
-        public ObservableCollection<TourRequestCardViewModel> SearchTourRequests(string country, string city, int numOfGuests,
-                                                                                 string language, DateTime minDate, DateTime maxDate)
-        {
-            var searchDateRange = new DateRange(minDate, maxDate);
-
-
-
-            var tourRequestCards = new ObservableCollection<TourRequestCardViewModel>();
-
-            foreach (var tourRequest in _tourRequestService.GetAllOnHold())
-            {
-                var requestDateRange = FindRequestDateRange(tourRequest);
-
-                if (!requestDateRange.IsOutOfRange(searchDateRange))
-                {
-                    var tourRequestCard = new TourRequestCardViewModel
-                    {
-                        Language = tourRequest.Language,
-                        NumOfGuests = tourRequest.MaxNumOfGuests,
-                        Location = tourRequest.Country + ", " + tourRequest.City,
-                        MinDate = new DateTime(tourRequest.MaintenanceStartDate.Year, tourRequest.MaintenanceStartDate.Month, tourRequest.MaintenanceStartDate.Day),
-                        MaxDate = new DateTime(tourRequest.MaintenanceEndDate.Year, tourRequest.MaintenanceEndDate.Month, tourRequest.MaintenanceEndDate.Day)
-                    };
-                    tourRequestCards.Add(tourRequestCard);
-                }
-            }
-
-            return tourRequestCards;
-        }
-
-        private DateRange FindRequestDateRange(TourRequest tourRequest)
-        {
-            var maintenanceStartDate = new DateTime(tourRequest.MaintenanceStartDate.Year,
-                tourRequest.MaintenanceStartDate.Month, tourRequest.MaintenanceStartDate.Day);
-
-            var maintenanceEndDate = new DateTime(tourRequest.MaintenanceEndDate.Year,
-                tourRequest.MaintenanceEndDate.Month, tourRequest.MaintenanceEndDate.Day);
-
-            var requestDateRange = new DateRange(maintenanceStartDate, maintenanceEndDate);
-
-            return requestDateRange;
-        }
     }
 }
