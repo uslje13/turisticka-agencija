12b70b5ba0b0b72ede13178b903c7ef8975053c5
somelijer <116906162+somelijer@users.noreply.github.com>
2023-06-01 22:45:50 +0200

    Adding models for forum

---
+++ TravelAgency/Domain/Models/ForumCommentReport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOSTeam.TravelAgency.Domain.Models
+{
+    public class ForumCommentReport
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CommentId { get; set; }
+
+        public ForumCommentReport(int id, int userId, int commentId)
+        {
+            Id = id;
+            UserId = userId;
+            CommentId = commentId;
+        }
+
+        public ForumCommentReport()
+        {
+            Id = -1;
+            UserId = -1;
+            CommentId = -1;
+        }
+    }
+}
