ae9a455012bd30fb6dc0a62cd720881c489b721d
Stefaan01 <116906194+Stefaan01@users.noreply.github.com>
2023-06-10 14:41:50 +0200

    pdf report
    have to close pdf before clicking of go back button

---
+++ TravelAgency/WPF/ViewModels/Guest1/ShowPDFViewModel.cs
@@ -0,0 +1,30 @@
+﻿using SOSTeam.TravelAgency.Commands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+using Syncfusion.PdfViewer;
+using Syncfusion.Pdf.Graphics;
+
+namespace SOSTeam.TravelAgency.WPF.ViewModels.Guest1
+{
+    public class ShowPDFViewModel
+    {
+        public NavigationService NavigationService { get; set; }
+        public RelayCommand GoBackCommand { get; set; }
+        public ShowPDFViewModel(NavigationService service)
+        {
+            NavigationService = service;
+
+            GoBackCommand = new RelayCommand(Execute_GoBack);
+        }
+
+        private void Execute_GoBack(object sender)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
---
+++ TravelAgency/WPF/Views/Guest1/ShowPDFPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using SOSTeam.TravelAgency.WPF.ViewModels.Guest1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SOSTeam.TravelAgency.WPF.Views.Guest1
+{
+    /// <summary>
+    /// Interaction logic for ShowPDFPage.xaml
+    /// </summary>
+    public partial class ShowPDFPage : Page
+    {
+        public ShowPDFPage(string pdfPath, NavigationService service)
+        {
+            InitializeComponent();
+            ShowPDFViewModel viewModel = new ShowPDFViewModel(service);
+            DataContext = viewModel;
+            FileStream stream = new FileStream(pdfPath, FileMode.Open);
+            PdfViewer.Load(stream);
+        }
+    }
+}
